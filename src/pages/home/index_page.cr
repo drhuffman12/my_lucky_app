7219a99416bd2308a43ff38d4b21d04e48ff966c
Daniel Huffman <drhuffman12@gmail.com>
2022-02-23 20:53:46 -0500

    Section "Writing Custom Queries" | "Adding a Query"

--- src/pages/home/index_page.cr
+++ src/pages/home/index_page.cr
@@ -1,5 +1,7 @@
 # class Home::IndexPage < MainLayout
 class Home::IndexPage < AuthLayout
+  needs fortunes : FortuneQuery
+
   def content
     div class: "px-4 py-5 my-5 text-center" do
       h1 "CloverApp", class: "display-5 fw-bold"
@@ -12,7 +14,16 @@ class Home::IndexPage < AuthLayout
       end
     end
     div class: "container" do
-      # we will use this later
+      div class: "list-group list-group-flush border-bottom scrollarea" do
+        fortunes.each do |fortune|
+          link to: Fortunes::Show.with(fortune.id), class: "list-group-item list-group-item-action py-3 lh-tight" do
+            div class: "d-flex w-100 align-items-center justify-content-between" do
+              small time_ago_in_words(fortune.created_at), class: "text-muted"
+            end
+            div fortune.text, class: "col-10 mb-1 small"
+          end
+        end
+      end
     end
   end
 end
